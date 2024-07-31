1349255891a4b2b3c33062887643d874ae8b8a3c
Elena <Shherbakove@yandex.ru>
2024-07-31 20:59:05 +0300

    семинар 8 работа с данными

---
+++ sem8_1.py
@@ -0,0 +1,7 @@
+import pandas as pd # импорт библиотеки для обработки и анализа данных
+import numpy as np # библиотека для работы с массивами данных
+import matplotlib.pyplot as plt # matplotlib.pyplot модуль для построения графиков
+import seaborn as sns # библиотека для визуализации данных, основана на matplotlib
+from sklearn.preprocessing import LabelEncoder # инструмент, для кодирования категориальных переменных
+from scipy import stats # библиотека для научных и математических вычислений
+
